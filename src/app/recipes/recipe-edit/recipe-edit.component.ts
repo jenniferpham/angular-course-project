157a4e35948a9313609847f5c9e34ead4d46dfa6
jenniferpham <j.pham87@gmail.com>
2017-08-14 23:56:05 -0700

    reactive form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -26,24 +26,47 @@ export class RecipeEditComponent implements OnInit {
       );
   }
 
+  onSubmit(){
+    console.log(this.recipeForm);
+  }
+
   private initForm(){
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if(this.editMode){
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if(recipe['ingredients']){
+        for (let ingredient of recipe.ingredients){
+           recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
     }
 
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
       'description': new FormControl(recipeDescription),
-      'ingredients': new FormArray([])
+      'ingredients': recipeIngredients
     })
   }
-
+  
+  onAddIngredients(){ //push more formControls into FormArray
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
 }
